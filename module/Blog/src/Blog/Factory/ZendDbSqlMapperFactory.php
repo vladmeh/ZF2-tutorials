3e59a6b5ee4839ad60c41417c8b14bb15d077dab
vladmeh <vladmeh@gmail.com>
2016-09-14 16:03:53 +0300

    Refactoring hidden dependencies. Finish.

--- module/Blog/src/Blog/Factory/ZendDbSqlMapperFactory.php
+++ module/Blog/src/Blog/Factory/ZendDbSqlMapperFactory.php
@@ -10,8 +10,10 @@
 namespace Blog\Factory;
 
 use Blog\Mapper\ZendDbSqlMapper;
+use Blog\Model\Post;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Stdlib\Hydrator\ClassMethods;
 
 class ZendDbSqlMapperFactory implements FactoryInterface
 {
@@ -24,6 +26,10 @@ class ZendDbSqlMapperFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        return new ZendDbSqlMapper($serviceLocator->get('Zend\Db\Adapter\Adapter'));
+        return new ZendDbSqlMapper(
+            $serviceLocator->get('Zend\Db\Adapter\Adapter'),
+            new ClassMethods(false),
+            new Post()
+        );
     }
 }
--- module/Blog/src/Blog/Mapper/ZendDbSqlMapper.php
+++ module/Blog/src/Blog/Mapper/ZendDbSqlMapper.php
@@ -14,6 +14,7 @@ use Zend\Db\Adapter\AdapterInterface;
 use Zend\Db\Adapter\Driver\ResultInterface;
 use Zend\Db\ResultSet\HydratingResultSet;
 use Zend\Db\Sql\Sql;
+use Zend\Stdlib\Hydrator\HydratorInterface;
 
 class ZendDbSqlMapper implements PostMapperInterface
 {
@@ -23,11 +24,30 @@ class ZendDbSqlMapper implements PostMapperInterface
     protected $dbAdapter;
 
     /**
-     * @param AdapterInterface  $dbAdapter
+     * @var \Zend\Stdlib\Hydrator\HydratorInterface
      */
-    public function __construct(AdapterInterface $dbAdapter)
+    protected $hydrator;
+
+    /**
+     * @var \Blog\Model\PostInterface
+     */
+    protected $postPrototype;
+
+    /**
+     * @param AdapterInterface  $dbAdapter
+     * @param HydratorInterface $hydrator
+     * @param PostInterface    $postPrototype
+     */
+    public function __construct(
+        AdapterInterface $dbAdapter,
+        HydratorInterface $hydrator,
+        PostInterface $postPrototype
+    )
     {
-        $this->dbAdapter = $dbAdapter;
+        $this->dbAdapter      = $dbAdapter;
+        $this->dbAdapter      = $dbAdapter;
+        $this->hydrator       = $hydrator;
+        $this->postPrototype  = $postPrototype;
     }
 
     /**
@@ -38,6 +58,18 @@ class ZendDbSqlMapper implements PostMapperInterface
      */
     public function find($id)
     {
+        $sql    = new Sql($this->dbAdapter);
+        $select = $sql->select('posts');
+        $select->where(array('id = ?' => $id));
+
+        $stmt   = $sql->prepareStatementForSqlObject($select);
+        $result = $stmt->execute();
+
+        if ($result instanceof ResultInterface && $result->isQueryResult() && $result->getAffectedRows()) {
+            return $this->hydrator->hydrate($result->current(), $this->postPrototype);
+        }
+
+        throw new \InvalidArgumentException("Blog with given ID:{$id} not found.");
     }
 
     /**
@@ -52,7 +84,7 @@ class ZendDbSqlMapper implements PostMapperInterface
         $result = $stmt->execute();
 
         if ($result instanceof ResultInterface && $result->isQueryResult()) {
-            $resultSet = new HydratingResultSet(new \Zend\Stdlib\Hydrator\ClassMethods(), new \Blog\Model\Post());
+            $resultSet = new HydratingResultSet($this->hydrator, $this->postPrototype);
 
             return $resultSet->initialize($result);
         }
